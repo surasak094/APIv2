1c24e7560e194bd5c645c32ccebdd7e57ac929b9
surasak094 <87976146+surasak094@users.noreply.github.com>
2021-08-25 14:30:26 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -31,18 +31,7 @@ with conn:
         model.fit(X_train,y_train)
         accuracy=model.score(X_train,y_train)
 
-        sql="select * from weatherv4"
-        dl=pd.read_sql(sql, conn)
-        date=dl['Date']
-        dl = dl[['MinTemp','MaxTemp','Rainfall','Evaporation','Sunshine','WindGustSpeed','WindSpeed9am','WindSpeed3pm','Humidity9am','Humidity3pm','Pressure9am','Pressure3pm','Cloud9am','Cloud3pm','Temp9am','Temp3pm','RainToday','RISK_MM']]       
-        dl['RainToday']=dl['RainToday'].map({'Yes':1,'No':0}).astype(int)         
-        y_predict=model.predict(dl)
-        print(y_predict)  
-        y_hat_test=model.predict(dl)         
-        dlive=pd.concat([date,dl,pd.Series(y_hat_test,name='predicted')],axis='columns')
-        dlive['predicted']=dlive['predicted'].map({1:'ตก',0:'ไม่ตก'}).astype(object)
-        print(dlive) 
-        print(accuracy)   
+        
           
 @app.route('/', methods=['GET','POST'])
 def hello():   
@@ -80,15 +69,29 @@ class Weather(Resource):
             j=json.loads(dh)
             print(json.dumps(j,indent=4))           
             return jsonify(j)
-class Weatherlive(Resource):    
+class Weatherlive(Resource):
+               
         def get(self):
-            dlive['predicted']=dlive['predicted'].map({'ตก':1,'ไม่ตก':0}).astype(int)
-            dh=dlive.to_json(orient='index')
-            j=json.loads(dh)
-            print(json.dumps(j,indent=4))
-            return jsonify(j)            
-
-
+            conn = pymysql.connect(db='heroku_78638205065f537', user='bc06134d8f3e1f', passwd='550425d9', host='us-cdbr-east-04.cleardb.com')
+            with conn:                
+                import pandas as pd                
+                sql="select * from weatherv4"
+                dl=pd.read_sql(sql, conn)
+                date=dl['Date']
+                dl = dl[['MinTemp','MaxTemp','Rainfall','Evaporation','Sunshine','WindGustSpeed','WindSpeed9am','WindSpeed3pm','Humidity9am','Humidity3pm','Pressure9am','Pressure3pm','Cloud9am','Cloud3pm','Temp9am','Temp3pm','RainToday','RISK_MM']]       
+                dl['RainToday']=dl['RainToday'].map({'Yes':1,'No':0}).astype(int)         
+                y_predict=model.predict(dl)
+                print(y_predict)  
+                y_hat_test=model.predict(dl)         
+                dlive=pd.concat([date,dl,pd.Series(y_hat_test,name='predicted')],axis='columns')
+                dlive['predicted']=dlive['predicted'].map({1:'ตก',0:'ไม่ตก'}).astype(object)
+                print(dlive) 
+                print(accuracy)
+                dlive['predicted']=dlive['predicted'].map({'ตก':1,'ไม่ตก':0}).astype(int)
+                dh=dlive.to_json(orient='index')
+                j=json.loads(dh)
+                print(json.dumps(j,indent=4))
+                return jsonify(j)            
 
 
 api.add_resource(Weather,"/Weather")
